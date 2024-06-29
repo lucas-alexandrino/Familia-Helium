ce8b6b71da7a1ba320b82994221f146e8b40a1d0
Lucas Alexandrino <lucas.alexcontato@gmail.com>
2024-06-29 01:12:08 -0300

    Familia HELIUM feito

--- src/assets/componentes/microsComponentes/containerLoginCdastro/ContainerEntrar.jsx
+++
@@ -1,8 +0,0 @@
-import Login from "../../pages/login/login"
-
-export default function ContainerEntrar()  {
-    return(
-    <div>
-        <Login/>
-    </div>)
-}
--- src/assets/componentes/pages/cadastro/Cadastro.jsx
+++
@@ -1,5 +0,0 @@
-const Cadastro = ()  => {
-
-}
-
-export default  Cadastro
